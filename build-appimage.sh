91b1ad4acd8437e6f9cdf98960f44980cf891f55
theUncanny <theUncanny@users.noreply.github.com>
2025-07-02 20:56:39 +0200

    Update build-appimage.sh to check and download appimagetool and linuxdeploy tools

--- build-appimage.sh
+++ build-appimage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 blc -build --release
-echo Creating application bundle
+echo "Creating application bundle"
 rm -r -f Tine.AppDir
 mkdir -p Tine.AppDir/usr/bin
 mkdir -p Tine.AppDir/usr/lib
@@ -11,6 +11,22 @@ cp Tine Tine.AppDir/usr/bin/
 cp icon/icon_256x256.png Tine.AppDir/usr/share/icons/hicolor/256x256/apps/Tine.png
 echo -e "[Desktop Entry]\nName=Tine\nExec=Tine\nIcon=Tine\nType=Application\nCategories=Utility\n" > Tine.AppDir/usr/share/applications/Tine.desktop
 # export NO_STRIP=true
+
+if [[ ! -f filename ]];
+then
+	echo "Downloading appimagetool..."
+	wget -c --content-disposition 'https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage' && \
+	chmod +x *.AppImage;
+fi
+
+if [[ ! -f filename ]];
+then
+	echo "Downloading linuxdeploy..."
+	wget -c --content-disposition 'https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage' && \
+	chmod +x *.AppImage;
+fi
+
 ./linuxdeploy-x86_64.AppImage --appdir Tine.AppDir --output appimage
 ./appimagetool-x86_64.AppImage Tine.AppDir
 
+chmod +x *.AppImage
